d2d3eb16ff7f7d46cc0551491692b39ad5a49d7c
Телицын Никита <dmtvn.labs@yandex.ru>
2022-09-11 22:05:17 +0300

    change docker-entrypoint

--- docker-entrypoint-initdb.d/0001_schema.sql
+++ docker-entrypoint-initdb.d/0001_schema.sql
@@ -1,2 +1,4 @@
 insert into users (full_name, email, password, role) values ('mr Robot', 'hello@hello.ru', 'hellohello', 'BOOKKEEPER');
+insert into users (full_name, email, password, role) values ('Arturov Artur Arturovic', 'hello1@hello.ru', 'hellohello', 'BOOKKEEPER');
 insert into users (full_name, email, password) values ('mr User', 'user@user.ru', 'hellohello');
+insert into users (full_name, email, password) values ('Ivanov Ivan Ivanovic', 'user1@user.ru', 'hellohello');
